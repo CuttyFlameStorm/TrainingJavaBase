f4853f590bd085d7ff12e737387fbc06a022e102
CuttyFlameStorm <cuttyflame.storm@gmail.com>
2023-07-14 02:05:38 +0200

    ajout de d'interface et class implementant ces interfaces génériques

---
+++ u_Base_de_la_generisite_training/src/genericite/MetierFournisseurImpl.java
@@ -0,0 +1,39 @@
+package genericite;
+
+import java.util.List;
+
+import InterfaceGenerique.IMetier;
+
+public class MetierFournisseurImpl implements IMetier<Fournisseur, String>{
+
+	@Override
+	public Fournisseur save(Fournisseur o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Fournisseur> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Fournisseur findOne(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Fournisseur o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(String id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- u_Base_de_la_generisite_training/src/methodeGenerique_dansUneClasseStandard/Produit.java
+++ u_Base_de_la_generisite_training/src/methodeGenerique_dansUneClasseStandard/Produit.java
@@ -5,8 +5,39 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class Produit implements Serializable{
 
+	private Long idProduit;
+	private String nomProduit;
+	private double prix;
+	
 	public Produit() {
 		
 	}
+
+	public Long getIdProduit() {
+		return idProduit;
+	}
+
+	public void setIdProduit(Long idProduit) {
+		this.idProduit = idProduit;
+	}
+
+	public String getNomProduit() {
+		return nomProduit;
+	}
+
+	public void setNomProduit(String nomProduit) {
+		this.nomProduit = nomProduit;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	
+	
+	
 
 }
